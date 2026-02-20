71e8b22dec89f4a3db778ac6dd30c6e3697bd6bf
Rohan Maharjan <rohanmhj9@gmail.com>
2026-02-20 20:48:57 +0545

    add button, display and delete button

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -3,33 +3,45 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+function App() {
+  const [input, setInput] =useState('')
+  const [todos, setTodos] = useState([])
+  const addTodo = () => {
+    if(input.trim() == "") return
+    setTodos([...todos, input])
+    setInput('')
+  }
+  const deleteTodo = (indexToDelete) => {
+    const updated = todos.filter((_, index) => index !== indexToDelete)
+    setTodos(updated)
+  }
+  return(
+    <div>
+      <h1>To-Do App</h1>
+    
+    <input
+      type='text'
+      value={input}
+      onChange= {(e) => setInput(e.target.value)} 
+      placeholder="Enter text"
+    />
+    <button onClick={addTodo}>Add</button>
+    <ul>
+      {todos.map((todo, index) => (
+        <li key={index}>{todo}</li>
+      ))}
+    </ul>
+    <ul>
+      {todos.map((todo, index) => (
+        <li key={index}>{todo}
+          <button onClick={() => deleteTodo(index)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+    </div>
   )
+    
 }
 
 export default App
